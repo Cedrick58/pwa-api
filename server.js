3cefa8082a5cb34bbe8e619d70204ae14d4d0c89
Cedrick <cedrick.moran.22e@utzmg.edu.mx>
2025-03-28 11:24:14 -0600

    fix2

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors());
 // Conexión a MongoDB
 mongoose.connect(
   'mongodb+srv://axel123:axel123@clustervite.q7s1q.mongodb.net/dbpwa?retryWrites=true&w=majority',
-  { useNewUrlParser: true, useUnifiedTopology: true }
+  { useNewUrlParser: true}
 )
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error(err));
